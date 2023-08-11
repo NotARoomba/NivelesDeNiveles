438e0107ae111dd7af11f3ab650f47bd5448a6a8
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-08-10 21:23:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const fs = require('fs').promises;
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline');
 const { read } = require('fs');
-const host = '127.0.0.1';
+const host = 'localhost';
 const port = 6606;
-const sport = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
-const parser = sport.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+// const sport = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
+// const parser = sport.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 data = "hi"
-parser.on('data',(e)=> {
-    data = e;
-})
+// parser.on('data',(e)=> {
+//     data = e;
+// })
 const requestListener = function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  
     res.writeHead(200);
     res.end(data);
 };
