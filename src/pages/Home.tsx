364e2f2bfa45c9068e477ab3f6e42453bd916635
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-08-23 20:16:21 -0500

    added permissions check and heatmap settings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   SafeAreaView,
@@ -7,15 +7,33 @@ import {
   ScrollView,
   Animated,
   RefreshControl,
+  Alert,
 } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {ScreenProp} from '../utils/DataTypes';
 import MapView, {PROVIDER_GOOGLE, Heatmap} from 'react-native-maps';
-import { check, PERMISSIONS } from 'react-native-permissions';
+import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
 
 export default function Home({fadeAnim, scale, isDarkMode}: ScreenProp) {
+  const [locationPerms, setLocationPerms] = useState(false);
   useEffect(() => {
     async function updateMap() {
+      const locationStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      if (locationStatus === RESULTS.GRANTED) {
+        setLocationPerms(true);
+      } else if (locationStatus === RESULTS.DENIED) {
+        const requestLocation = await request(
+          PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+        );
+        if (requestLocation === RESULTS.GRANTED) {
+          setLocationPerms(true);
+        } else {
+          Alert.alert(
+            'Activa Ubicacion',
+            'Niveles de Niveles nessecita tu ubicacion para asegurar que no estas en riesgo!',
+          );
+        }
+      }
       SplashScreen.hide();
     }
     updateMap();
@@ -26,6 +44,9 @@ export default function Home({fadeAnim, scale, isDarkMode}: ScreenProp) {
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
     async function updateMap() {
+      setLocationPerms(
+        (await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)) === RESULTS.GRANTED,
+      );
       setRefreshing(false);
     }
     updateMap();
@@ -43,26 +64,32 @@ export default function Home({fadeAnim, scale, isDarkMode}: ScreenProp) {
             <Text className="text-neutral-200 bg-neutral-800 text-center text-4xl py-5">
               Insert Logo here
             </Text>
-            {check(PERMISSIONS.IOS.LOCATION_ALWAYS)}
-            <MapView
-              className="w-screen aspect-square bg-neutral-200 justify-center m-auto"
-              provider={PROVIDER_GOOGLE}
-              showsUserLocation
-              showsMyLocationButton
-              loadingEnabled
-              initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}>
-              <Heatmap
-                points={[
-                  {latitude: 37.78825, longitude: -122.4324, weight: 10000},
-                  {latitude: 37.78825, longitude: -122.44, weight: 10000},
-                ]}
-              />
-            </MapView>
+            {locationPerms ? (
+              <MapView
+                className="w-screen aspect-square bg-neutral-200 justify-center m-auto"
+                provider={PROVIDER_GOOGLE}
+                showsUserLocation
+                showsMyLocationButton
+                loadingEnabled
+                initialRegion={{
+                  latitude: 37.78825,
+                  longitude: -122.4324,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}>
+                <Heatmap
+                  points={[{latitude: 37.7882, longitude: -122.4324}]}
+                  radius={300}
+                  gradient={{
+                    colorMapSize: 1000,
+                    startPoints: [0.1, 0.5, 1],
+                    colors: ['#008000', '#FFA500', '#FF0000'],
+                  }}
+                />
+              </MapView>
+            ) : (
+              <></>
+            )}
           </View>
         </ScrollView>
       </SafeAreaView>
