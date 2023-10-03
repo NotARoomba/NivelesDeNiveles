334bc1bc10dd1d3e4c4df8c4511cb3294eb42dcb
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-10-03 07:09:43 -0500

    Create arduino.ino

---
+++ arduino/arduino.ino
@@ -0,0 +1,87 @@
+// C++ code
+//
+
+enum STATUS {
+  SAFE,
+  RISK,
+  DANGER
+};
+
+enum TYPE {
+  FLOOD,
+  FIRE,
+  AVALANCHE
+};
+
+
+const int TRIGGER_PIN = 3;
+const int ECHO_PIN = 2; 1
+const int SENSOR_TYPE = 0;
+const int MAX_ULTRASONIC_DISTANCE = 330;
+const float ULTRASONIC_STATUS_DISTANCE = MAX_ULTRASONIC_DISTANCE / 3;
+const int PAST_DISTANCES_SIZE = 15;
+
+STATUS status = SAFE;
+long pastDistances[PAST_DISTANCES_SIZE];
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop()
+{
+  // clear trigger
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  // tell sensor to send sound stuff
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  // read echo data
+  float duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration*0.034/2;
+  
+  // VERY IMPORTANT CHECK FOR BIRDS - DONT DELETE
+  int distanceArrLength = 0;
+  for (int i = 0; i < PAST_DISTANCES_SIZE; i++) {
+    if (pastDistances[i] != 0) distanceArrLength++;
+  }
+  
+  if (distanceArrLength == PAST_DISTANCES_SIZE) {
+    for (int i = 0; i < PAST_DISTANCES_SIZE-1; i++) {
+      pastDistances[i] = pastDistances[i + 1];
+    }
+    pastDistances[PAST_DISTANCES_SIZE-1] = distance;
+  } else {
+    pastDistances[distanceArrLength] = distance;
+  };
+  
+  // VERY IMPORTANTE AVERAGE FOR BIRDS - DONT DELETE
+  float averageDistance = 0.0f;
+  for (int i = 0; i < PAST_DISTANCES_SIZE; i++) {
+    averageDistance += pastDistances[i];
+  }
+  averageDistance /= PAST_DISTANCES_SIZE;
+  
+  
+  if (averageDistance > (ULTRASONIC_STATUS_DISTANCE * 2) || distanceArrLength != PAST_DISTANCES_SIZE) {
+    status = SAFE;
+  } else if(averageDistance > ULTRASONIC_STATUS_DISTANCE) {
+    status = RISK;
+  } else {
+    status = DANGER;
+  }
+  
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  Serial.print("Status: ");
+  Serial.println(status);
+  Serial.print("Average Distanve: ");
+  Serial.println(averageDistance);
+
+  
+  delay(100);
+}
