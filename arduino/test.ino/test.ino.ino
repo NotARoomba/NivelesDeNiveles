82a542d8da2c5e544110c58dd530df128abd7b98
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-10-06 17:45:59 -0500

    Update test.ino.ino

--- arduino/test.ino/test.ino.ino
+++ arduino/test.ino/test.ino.ino
@@ -1,75 +1,41 @@
 String datos;
 #include <SoftwareSerial.h>
-#include <TinyGPS++.h>
 
-SoftwareSerial GPS(3, 2);
 
-#define RX 3  //Define A4 as RX
-#define TX 2  //Define A5 as TX
-#define PWK 7  //pin 8 must be sett HIGH in order to power on the module POWERKEY ON
-#define ENABLE 0  //pin 10 must be HIGH in order to communicate with M95
-char text[150];   
-String message="";
-int i;
-   
+#define RX 3  
+#define TX 2 
+#define ST 6
+#define GPS_RST 8
+#define PWK 7  //pin 7 must be sett HIGH in order to power on the module POWERKEY ON
+
+
+SoftwareSerial GPS(TX, RX);
 
 void setup() {
   Serial.begin(9600);
   GPS.begin(9600);
   pinMode(PWK, OUTPUT);
-  pinMode(ENABLE, OUTPUT);
   digitalWrite(PWK, HIGH);
-  digitalWrite(ENABLE, HIGH);
-  
-  while(!Serial){  //waiting for Serial to be TRUE
-    ;
-  }
-  Serial.println("Serial communikation is open");
-  
-  GPS.begin(57600);
-  delay(10);
-  Serial.println("Serial com via M95 is now opend!");
+  pinMode(GPS_RST, OUTPUT);
+  digitalWrite(GPS_RST, HIGH);
   delay(5000);
-  GPS.println("AT+CNMI=1,2,0,0,0"); // Decides how newly arrived SMS messages should be handled
-
-  Serial.println("Write your SMS:"); 
-  
+  // connect sim
+//  GPS.println("AT+COPS=1,0,\"CLARO\"");
+// open tcp connection to server
+//    GPS.println("AT+QIOPEN=\"TCP\",\"nivelesdeniveles-api.notaroomba.xyz\",3001");
+// register for registration
+//GPS.print.n("AT+CREG=2");
+  Serial.println("Open for commands!");
 }
 
-void loop(){   
-
-i=0;   
-while( Serial.available()>0 ){     
- text[i] = Serial.read();   
- message += text[i];   
- i++;   
- if (text[i-1]==10){  
-  Serial.println("Send SMS......");  
-  SendTextMessage();  
-  ShowSerialData();  
-  delay(1000);   
-  Serial.println("Success");   
-  message="";  
-  i=0;  }}
-  ShowSerialData();  
-  delay(1000);
-}  
-
-
-void SendTextMessage(){ 
+void loop(){
   
-GPS.print("AT+CMGF=1\r"); 
-delay(1000);  
-GPS.print("AT+CMGS=\"+573104250018\"\r");  //Change this number to landcode+phone
-delay(1000);  
-GPS.println(message);  
-GPS.print("\r");  
-delay(1000);  
-GPS.write(26);  
- }  
+   if (GPS.available()) {
+    Serial.println(GPS.readString());
+   }
+   if (Serial.available()) {
+    GPS.write(Serial.read());
+   }
+   
 
-
-void ShowSerialData(){  
-
-while(GPS.available()!=0)  
- Serial.write(GPS.read());}
+}
