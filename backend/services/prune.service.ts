bd82177669d9a70574c311b2dd45f33a853ee9e3
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-10-08 16:37:19 -0500

    Update prune.service.ts

--- backend/services/prune.service.ts
+++ backend/services/prune.service.ts
@@ -1,6 +1,8 @@
-import {Server} from 'socket.io';
 import Report from '../models/report';
 import {collections} from './database.service';
+import Sensor from '../models/sensor';
+import Incident from '../models/incident';
+import { DangerLevel } from '../models/types';
 
 export default async function prune() {
   // loop through all the sensors and check for any anomalies, adding each one to an incident that is close to the sensor OR make a new incident if there is no previous incident nearby, the number of reports (weight) should be a few times greater thena the users
@@ -12,12 +14,61 @@ export default async function prune() {
   // TODO: repurpouse function to sendIncidents where takes in a socket and then gets all the incidents and for each user it emits a message to the user depending on the level of the incident or if it has changed
 
   // GREATER THAN 2 HOURS WILL BE MARKED AS OVER AND NOT PROCESSED ANYMORE
-  await collections.reports?.updateMany(
-    {timestamp: {$lt: Date.now() - (1000 * 3600 * 2)}},
-    {$set: {over: true}},
-  );
+  // IF AND ONlY IF THERE ARE NO SENSORS NEARBY THAT SAY OTHERWISE
+  const activeReports =  collections.reports?.find(
+    { timestamp: { $lt: Date.now() - (1000 * 3600 * 2) } }) as unknown as Report[];
+  for (let report of activeReports) {
+    const sensorsNearby: Sensor[] = (await collections.sensors
+      ?.find({
+        location: {
+          $near: {
+            $geometry: {...report.location},
+            $maxDistance: 2000,
+          },
+        },
+        over: false,
+        type: report.type,
+      })
+      .toArray()) as unknown as Sensor[];
+      if (sensorsNearby.length !== 0) {
+        await collections.reports?.updateOne({location: report.location}, {$set: {timestamp: Date.now()}})
+      }
+  }
+  // INCIDENTS
+  // for every incident get the sensor that is within range of the incident and if it is of the same type then set the incident to the current date.now
+  const activeIncidents =  collections.incidents?.find(
+    { timestamp: { $lt: Date.now() - (1000 * 3600 * 2) } }) as unknown as Incident[];
+    for (let incident of activeIncidents) {
+      const sensorsNearby: Sensor[] = (await collections.sensors
+        ?.find({
+          location: {
+            $near: {
+              $geometry: {...incident.location},
+              $maxDistance: incident.range,
+            },
+          },
+          over: false,
+          type: incident.type,
+        })
+        .toArray()) as unknown as Sensor[];
+      if (sensorsNearby.length !== 0) {
+        for (let sensor of sensorsNearby) {
+          //check for if the sensors havent updated in a while
+          if (sensor.status !== DangerLevel.SAFE) {
+            await collections.incidents?.updateOne({location: incident.location}, {$set: {timestamp: Date.now()}})
+          }
+        }
+      }
+    }
+  // need to get all the sensors within a certain radius and if the report matches the type of sensor then set the active report timespamp to now
+    //then set over the ones not updated
+    await collections.reports?.updateMany(
+      {timestamp: {$lt: Date.now() - (1000 * 3600 * 2)}},
+      {$set: {over: true}},
+    );
   await collections.incidents?.updateMany(
     {timestamp: {$lt: Date.now() - (1000 * 3600 * 2)}},
     {$set: {over: true}},
   );
 }
+
