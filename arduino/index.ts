688642bdecbf4c8fa97fce94498bff8b42e9479a
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-10-08 13:25:48 -0500

    hmac finally working

--- arduino/index.ts
+++ arduino/index.ts
@@ -1,5 +1,5 @@
 import os from 'os';
-import crypto from 'crypto'
+import CryptoJS from 'crypto-js';
 import {ReadlineParser} from '@serialport/parser-readline';
 import {SerialPort, SerialPortOpenOptions} from 'serialport';
 import * as dotenv from 'ts-dotenv';
@@ -34,42 +34,28 @@ parser.on('data', async (data: string) => {
   if (json && json.status !== beforeStatus) {
     beforeStatus = json.status;
     console.log(`STATUS CHANGED TO ${json.status}!`);
-    console.log(data)
+    // console.log(data)
     // post data to api
-    // const time = Date.now().toString();
-    // const digest = CryptoJS.enc.Hex.stringify(
-    //   CryptoJS.HmacSHA256(
-    //     time + 'POST' + '/sensors' + CryptoJS.MD5(data).toString(),
-    //     Math.floor(Date.now() / (30 * 1000)).toString(),
-    //   ),
-    // );
-    // const hmac = `HMAC ${time}:${digest}`;
-    // const hmac = crypto.createHmac("sha256", Math.floor(Date.now() / (30 * 1000)).toString());
-    // const time = Date.now().toString();
+    const time = Date.now().toString();
+    const digest = CryptoJS.enc.Hex.stringify(
+      CryptoJS.HmacSHA256(
+        time +
+          'POST' +
+          '/sensors' +
+          CryptoJS.MD5(JSON.stringify(json)).toString(),
+        Math.floor(Date.now() / (30 * 1000)).toString(),
+      ),
+    );
+    const hmac = `HMAC ${time}:${digest}`;
 
-    // hmac.update(time);
-    // hmac.update("POST");
-    // hmac.update("/sensors/");
-
-    // const contentHash = crypto.createHash("md5");
-    // contentHash.update(data);
-
-    // hmac.update(contentHash.digest("hex"));
-
-    const res = await fetch(env.API_URL + '/sensors', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        //   Authorization: `HMAC ${time}:${hmac.digest("hex")}`,
-        },
-        body: data,
-      })
-    console.log(res.json);
-    // if (res.error) {
-    //   console.log(`ERROR: ${res.msg ?? res.info}`);
-    // } else {
-    //   console.log('Succesfully sent data!');
-    // }
+    fetch(env.API_URL + '/sensors', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: hmac,
+      },
+      body: JSON.stringify(json),
+    });
   }
 });
