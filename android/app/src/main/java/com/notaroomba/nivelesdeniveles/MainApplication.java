f20087cddf274dd47036e7aaf066e95521f20117
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-11-18 00:55:08 -0500

    android build stuff

--- android/app/src/main/java/com/notaroomba/nivelesdeniveles/MainApplication.java
+++ android/app/src/main/java/com/notaroomba/nivelesdeniveles/MainApplication.java
@@ -30,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new RNFSPackage());
+            packages.add(new BackgroundGeolocationPackage());
           return packages;
         }
 
