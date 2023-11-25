77680b85e153599c6099777ec79899e3a87ef379
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-11-25 12:34:43 -0500

    added stuff

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -1,7 +1,7 @@
-import React, {View, Text, TouchableOpacity, Animated} from 'react-native';
+import React, {View, Text, TouchableOpacity, Animated, Image, Modal} from 'react-native';
 import {AdviceProps, DangerLevel} from '../utils/Types';
 import Icon from 'react-native-vector-icons/Feather';
-import {useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {Localizations} from '../utils/Localizations';
 
 export default function Advice({status, isOpen, setOpen}: AdviceProps) {
@@ -36,6 +36,11 @@ export default function Advice({status, isOpen, setOpen}: AdviceProps) {
       useNativeDriver: false,
     }).start();
   };
+  const title = useState('');
+  const description = useState('');
+  useEffect(() => {
+
+  }, [status])
   return (
     <TouchableOpacity className="mt-3" onPress={changeOpen}>
       <View className="bg-highlight p-2 py-3 rounded-xl w-11/12 m-auto justify-center">
@@ -57,8 +62,42 @@ export default function Advice({status, isOpen, setOpen}: AdviceProps) {
             <Icon name="chevron-down" size={30} />
           </Animated.View>
         </View>
-        <Animated.View
-          style={{height: growValue /*, maxHeight: grow*/}}
+        <Modal
+      animationType="fade"
+      visible={isOpen}
+      style={{backgroundColor: '#000000'}}
+      transparent
+      onRequestClose={() => {
+        setOpen(!isOpen);
+      }}>
+      <View className="flex justify-center bg-light/70 h-screen">
+        <View className="flex jutify-center align-middle m-auto bg-light w-9/12 rounded-xl shadow-xl">
+          <Image
+            source={require('../../public/icon.png')}
+            className="h-32 aspect-square mx-auto mt-4"
+          />
+          <View className="flex flex-col">
+            <Text className="m-auto mt-2 text-2xl font-bold text-dark  ">
+              {title}
+            </Text>
+            <Text className="m-auto mt-2 text-black text-center text-lg my-2 mb-8 px-8">
+              {description}
+            </Text>
+          </View>
+          <View className="flex flex-row justify-center gap-4 mb-8">
+            <TouchableOpacity
+              onPress={() => setOpen(!isOpen)}
+              className=" bg-dark  flex justify-center align-middle p-2 rounded w-32">
+              <Text className="text-xl text-light m-auto font-bold">
+                {Localizations.done}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+        {/* <Animated.View
+          style={{height: growValue}}
           className="justify-center px-1">
           <Text className="text-dark text-left text-lg">
             {status === DangerLevel.SAFE
@@ -67,7 +106,7 @@ export default function Advice({status, isOpen, setOpen}: AdviceProps) {
               ? Localizations.recommendationsForRisk
               : Localizations.recommendationsForDanger}
           </Text>
-        </Animated.View>
+        </Animated.View> */}
       </View>
     </TouchableOpacity>
   );
