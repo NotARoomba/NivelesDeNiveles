6307f823705324da1174a80bd7af3f785087ff9a
Nathan <70660308+NotARoomba@users.noreply.github.com>
2024-11-03 10:26:17 -0500

    rocket start

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,12 +4,14 @@ use axum::{
 };
 use std::env;
 use dotenv::dotenv;
+mod database;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // let collections = database::init_database().await.unwrap();
+    let app = Router::new().route("/", get(|| async { "You're not supposed to be here!" }));
     let port = env::var("PORT").unwrap_or("3000".to_string());
     // run our app with hyper, listening globally on the port
     println!("Server running on port {}", port);
---
+++ backend/src/routes/verify.rs
@@ -0,0 +1,3 @@
+use axum::routing;
+
+use crate::database::Collections;
